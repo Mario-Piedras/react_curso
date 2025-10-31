c0b61867db68a4dfbd233e8af8411431bce6d177
pacheco333 <santiago2006ortizp@gmail.com>
2025-10-31 09:59:55 -0500

    carpetas de react

---
+++ ex_01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <p>Welcome to my Ract application.</p>
+    </>
+  )
+}
+
+export default App
